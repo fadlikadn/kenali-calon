a60e7c04c5308ff9bb155f1efbab04777909d9b6
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2024-02-06 10:16:21 +0700

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from './providers'
 import { ThemeModeScript } from 'flowbite-react'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
